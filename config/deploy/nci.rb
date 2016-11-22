b94b5d1c6a9edf878dbb724bf5a789d2c7ec50e9
Andrew Foster <andy@sol1.com.au>
2016-11-22 21:12:17 +1100

    Deploy config

--- config/deploy/nci.rb
+++ config/deploy/nci.rb
@@ -1,8 +1,8 @@
 # Your HTTP server, Apache/etc
-role :web, 'alveo-nci.sol1.net'
+role :web, 'app.alveo.edu.au'
 # This may be the same as your Web server
-role :app, 'alveo-nci.sol1.net'
+role :app, 'app.alveo.edu.au'
 # This is where Rails migrations will run
-role :db,  'alveo-nci.sol1.net', :primary => true
+role :db,  'app.alveo.edu.au', :primary => true
 
-set :server_url, "http://alveo-nci.sol1.net"
+set :server_url, "https://app.alveo.edu.au/"
